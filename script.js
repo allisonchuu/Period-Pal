aac568b66a3215db592e05818f0d4ee1136c0096
Allison Chu <allisonchu@Allisons-MBP-2.lan>
2022-02-19 21:34:50 -0800

    Added game page

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-var a = document.getElementById("titlepage");
 
-a.addEventListener("keydown", function(e) {
-  if (e.key == 13) {
-    window.location.href = "game.html";
-  }
+
+document.querySelector("#titlepage").addEventListener("keyup", event => {
+  if (event.key !== "Enter") return;
+  window.location = "game.html";
+  event.preventDefault();
 });
-          
+
+document.querySelector("#titlegame").addEventListener("click", function () {
+  window.location = "index.html";
+});
